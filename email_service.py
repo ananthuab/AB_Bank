761f83165cd7cef45e54aca26c86ee0b92550472
Ananthakrishnan B <151920921+ananthuab@users.noreply.github.com>
2023-12-12 20:21:40 +0400

    email_service.py

--- email_service.py
+++ email_service.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, jsonify
 import smtplib
 from email.message import EmailMessage
+import json
 
 app = Flask(__name__)
 
 class EmailService:
     def send_email(self, user_email, pdf_path):
         # Add your email configuration here
-        email_address = "ananthu@gmail.com"
-        email_password = "ananthupass"
+        email_address = "<ananthakrishnanb21@gmail.com>"
+        email_password = "<rhzgxxvwzfhorovz>"
 
         msg = EmailMessage()
         msg.set_content("Please find attached your bank statement.")
@@ -19,17 +20,21 @@ class EmailService:
         with open(pdf_path, 'rb') as file:
             msg.add_attachment(file.read(), maintype='application', subtype='pdf', filename='bank_statement.pdf')
 
-        with smtplib.SMTP('smtp.gmail.com', 587) as server:
-            server.starttls()
-            server.login(email_address, email_password)
-            server.send_message(msg)
+
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(email_address, email_password)
+        server.sendmail(email_address, user_email, msg.as_string())
+        server.close()
+        print ('successfully sent the mail')
 
 @app.route('/send-email', methods=['POST'])
 def send_email():
-    data = request.get_json()
+    data = json.loads(request.data, strict=True)
+    print(data)
     user_email = data.get('user_email')
     pdf_path = data.get('pdf_path')
-
     EmailService().send_email(user_email, pdf_path)
 
     return jsonify({'message': 'Email sent successfully'})
